b86974e7a65493d823d90d68989c31469b12b5aa
BoredDany <martinezq.d@javeriana.edu.co>
2024-04-10 22:08:40 -0500

    main only

---
+++ app/src/main/java/com/example/tallermovil/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tallermovil
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val camBtn = findViewById<ImageButton>(R.id.camBtn)
+        val contactsBtn = findViewById<ImageButton>(R.id.contactBtn)
+        val mapBtn = findViewById<ImageButton>(R.id.mapBtn)
+
+        camBtn.setOnClickListener { }
+        contactsBtn.setOnClickListener {  }
+        mapBtn.setOnClickListener {  }
+
+    }
+}
